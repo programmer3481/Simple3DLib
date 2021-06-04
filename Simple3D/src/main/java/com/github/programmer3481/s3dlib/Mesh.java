773ce5fabd7939069278e718a6688af3b0b6505f
programmer3481 <55805604+programmer3481@users.noreply.github.com>
2021-06-05 01:20:54 +0900

    update gitignore and move primitive shape generation to mesh class

--- Simple3D/src/main/java/com/github/programmer3481/s3dlib/Mesh.java
+++ Simple3D/src/main/java/com/github/programmer3481/s3dlib/Mesh.java
@@ -4,6 +4,8 @@ import org.lwjgl.system.MemoryUtil;
 
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
+import org.joml.Vector3f;
+import org.joml.Vector2f;
 
 import static org.lwjgl.opengl.GL11.GL_FLOAT;
 import static org.lwjgl.opengl.GL15.*;
@@ -130,5 +132,44 @@ public class Mesh {
 
     public int getIBO() {
         return ibo;
+    }
+
+    public Mesh Cube() {
+        return new Mesh(new Vertex[] {
+            new Vertex(new Vector3f(-1.0f, -1.0f, -1.0f), new Vector2f(1.0f, 2.0f/4)), //v0
+            new Vertex(new Vector3f(1.0f, -1.0f, -1.0f), new Vector2f(2.0f/3, 2.0f/4)), //v1
+            new Vertex(new Vector3f(1.0f, 1.0f, -1.0f), new Vector2f(2.0f/3, 3.0f/4)), //v2
+            new Vertex(new Vector3f(-1.0f, 1.0f, -1.0f), new Vector2f(1.0f, 3.0f/4)), //v3
+            new Vertex(new Vector3f(-1.0f, -1.0f, 1.0f), new Vector2f(0.0f, 2.0f/4)), //v4
+            new Vertex(new Vector3f(1.0f, -1.0f, 1.0f), new Vector2f(1.0f/3, 2.0f/4)), //v5
+            new Vertex(new Vector3f(1.0f, 1.0f, 1.0f), new Vector2f(1.0f/3, 3.0f/4)), //v6
+            new Vertex(new Vector3f(-1.0f, 1.0f, 1.0f), new Vector2f(0.0f, 3.0f/4)), //7
+
+            new Vertex(new Vector3f(-1.0f, -1.0f, -1.0f), new Vector2f(2.0f/3, 1.0f/4)), //v8
+            new Vertex(new Vector3f(-1.0f, 1.0f, -1.0f), new Vector2f(2.0f/3, 1.0f)), //v9
+            new Vertex(new Vector3f(-1.0f, 1.0f, 1.0f), new Vector2f(1.0f/3, 1.0f)), //10
+            new Vertex(new Vector3f(-1.0f, -1.0f, 1.0f), new Vector2f(1.0f/3, 1.0f/4)), //v11
+
+            new Vertex(new Vector3f(-1.0f, 1.0f, -1.0f), new Vector2f(2.0f/3, 0.0f)), //v12
+            new Vertex(new Vector3f(-1.0f, 1.0f, 1.0f), new Vector2f(1.0f/3, 0.0f)) //13
+    }, new int[] {
+            5, 2, 6,
+            5, 1, 2,
+
+            4, 6, 7,
+            4, 5, 6,
+
+            1, 3, 2,
+            1, 0, 3,
+
+            6, 9, 10,
+            6, 2, 9,
+
+            1, 11, 8,
+            1, 5, 11,
+
+            8, 13, 12,
+            8, 11, 13
+    });
     }
 }
